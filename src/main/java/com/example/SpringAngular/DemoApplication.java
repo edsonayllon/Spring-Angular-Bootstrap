063ce9a9a8dc48f2c1f1a40811fc66882537d76e
Edson Ayllon <eayllon1@gmail.com>
2020-05-06 21:27:09 -0500

    project

--- src/main/java/com/example/SpringAngular/DemoApplication.java
+++ src/main/java/com/example/SpringAngular/DemoApplication.java
@@ -1,18 +1,10 @@
-package com.example.server;
+package com.example.SpringAngular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/")
-	public String greet(){
-		return "welcome!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/test/java/com/example/SpringAngular/DemoApplicationTests.java
+++ src/test/java/com/example/SpringAngular/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.server;
+package com.example.SpringAngular;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
